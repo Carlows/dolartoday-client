56de3c36a0d6a7d4a378254bcde0199f28fe2472
Carlos Martinez <carlos@hashlabs.com>
2017-06-04 21:56:24 -0400

    add format

--- src/DolarTodayChart.js
+++ src/DolarTodayChart.js
@@ -1,15 +1,19 @@
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 import React, { Component } from 'react';
 
+const formatBs = (value) => {
+    return `${value} Bs`;
+} ;
+
 const DolarTodayChart = ({data}) => {
   return (
     <LineChart
-      width={720} height={250} data={data}
+      width={900} height={350} data={data}
       margin={{ top: 20, right: 50, left: 20, bottom: 5 }}>
-      <XAxis dataKey="day"/>
-      <YAxis type="number" axisLine={false} />
+      <XAxis dataKey="day" />
+      <YAxis type="number" axisLine={false} tickFormatter={formatBs} />
       <Tooltip/>
-      <CartesianGrid strokeDashArray="3 3"/>
+      <CartesianGrid strokeDasharray="3 3"/>
       <Line type="monotone" dataKey="minPrice" stroke="#8884d8" activeDot={{r: 8}} />
       <Line type="monotone" dataKey="maxPrice" stroke="#e62020" activeDot={{r: 8}} />
     </LineChart>
